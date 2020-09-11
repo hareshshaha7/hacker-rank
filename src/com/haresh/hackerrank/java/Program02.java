474d8760cd9aa396bcb5209c2237ab26c290b374
hareshshaha7 <haresh.shaha7@gmail.com>
2020-09-11 11:01:37 +0530

    Covered all test cases.

--- src/com/haresh/hackerrank/java/Program02.java
+++ src/com/haresh/hackerrank/java/Program02.java
@@ -9,17 +9,21 @@ import java.util.*;
 
 public class Program02 {
 
-	    public static void main(String[] args) {
-	        Scanner scan = new Scanner(System.in);
-	        String s = scan.nextLine();
-	        // Write your code here.
-	        String regex = "[ !,?._'@]+";
-	        String[] tokens = s.split(regex);
-	        System.out.println(tokens.length);
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String s = scan.nextLine().trim();
+		// Write your code here.
+		if (!s.isEmpty()) {
+			String regex = "[ !,?\\\\.\\\\_'@]+";
+			String[] tokens = s.split(regex);
+			System.out.println(tokens.length);
 
-	        for (String token : tokens)
-	            System.out.println(token);
-	        scan.close();
-	    }
+			for (String token : tokens)
+				if (token != null)
+					System.out.println(token);
+		} else
+			System.out.println(0);
+
+		scan.close();
 	}
-
+}
