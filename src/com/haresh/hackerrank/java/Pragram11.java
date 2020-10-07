a0951c65c5fb439c2e04b4bcd4172d600061669d
hareshshaha7 <haresh.shaha7@gmail.com>
2020-10-07 18:36:43 +0530

    Correct solution

--- src/com/haresh/hackerrank/java/Pragram11.java
+++ src/com/haresh/hackerrank/java/Pragram11.java
@@ -9,37 +9,36 @@ import java.util.*;
 
 public class Pragram11 {
 
-    public static boolean canWin(int leap, int[] game) {
-        // Return true if you can win the game; otherwise, return false.
-        boolean canWalk = true;
-        int index = 0;
-        while(canWalk) {
-            if((index + leap) >= game.length)
-                return true;
-            if (game[index + leap] == 0)
-                index = index + leap;
-            else if(game[index + 1] == 0) 
-                index++;
-            else if (game[index + 1] != 0 && game[index + leap] != 0)
-                canWalk = false;
-        }
-        return false;
-    }
+	public static boolean canWin(int leap, int[] game) {
+		// Return true if you can win the game; otherwise, return false.
+		return isSolvable(leap, game, 0);
+	}
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int q = scan.nextInt();
-        while (q-- > 0) {
-            int n = scan.nextInt();
-            int leap = scan.nextInt();
-            
-            int[] game = new int[n];
-            for (int i = 0; i < n; i++) {
-                game[i] = scan.nextInt();
-            }
+	private static boolean isSolvable(int leap, int[] game, int i) {
+		if (i >= game.length)
+			return true;
+		else if (i < 0 || game[i] == 1)
+			return false;
 
-            System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
-        }
-        scan.close();
-    }
+		game[i] = 1;
+
+		return isSolvable(leap, game, i + leap) || isSolvable(leap, game, i + 1) || isSolvable(leap, game, i - 1);
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int q = scan.nextInt();
+		while (q-- > 0) {
+			int n = scan.nextInt();
+			int leap = scan.nextInt();
+
+			int[] game = new int[n];
+			for (int i = 0; i < n; i++) {
+				game[i] = scan.nextInt();
+			}
+
+			System.out.println((canWin(leap, game)) ? "YES" : "NO");
+		}
+		scan.close();
+	}
 }
