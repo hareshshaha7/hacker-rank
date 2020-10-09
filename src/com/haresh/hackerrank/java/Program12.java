167c165621469ab03ea60aab6e5b6e7242cef1c2
hareshshaha7 <haresh.shaha7@gmail.com>
2020-10-09 11:57:51 +0530

    Faster solution

--- src/com/haresh/hackerrank/java/Program12.java
+++ src/com/haresh/hackerrank/java/Program12.java
@@ -6,27 +6,35 @@ package com.haresh.hackerrank.java;
 */
 
 import java.util.*;
-public class Program12 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        Deque<Integer> deque = new LinkedList<Integer>();
-        int n = in.nextInt();
-        int m = in.nextInt();
-        int maxUniqueInt = 0;
-        
-        for (int i = 0; i < n; i++) {
-            int num = in.nextInt();
-            if (deque.size() == m)
-            	deque.pollFirst();
 
-            if (!deque.contains(num)) 
-            	deque.offerLast(num);
-            
-            if (maxUniqueInt < deque.size())
-            	maxUniqueInt = deque.size();
-        }
-        System.out.println(maxUniqueInt);
-        
-        in.close();
-    }
+public class Program12 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		Deque<Integer> deque = new ArrayDeque<Integer>();
+		int n = in.nextInt();
+		int m = in.nextInt();
+		
+		int maxUniqueInt = 0;
+		Set<Integer> set = new HashSet<Integer>();
+
+		for (int i = 0; i < n; i++) {
+			int num = in.nextInt();
+			
+			deque.offerLast(num);
+			set.add(num);
+				
+			if (deque.size() == m) {
+				if (maxUniqueInt < set.size())
+					maxUniqueInt = set.size();
+				
+				int removedNumber = deque.pollFirst();
+				if (!deque.contains(removedNumber))
+					set.remove(removedNumber);
+			}
+			
+		}
+		System.out.println(maxUniqueInt);
+
+		in.close();
+	}
 }
